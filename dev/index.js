96cef78e0ddb7e1b950308f06fe29961a91c9d5a
ASxx <123456789@qq.com>
2022-03-09 11:14:43 +0800

    3333

--- dev/index.js
+++ dev/index.js
@@ -5,3 +5,15 @@ const config={
     publish
 }
 export default config.dev  //设置测试环境
+const TestCafe = require('testcafe');
+const testCafe = await TestCafe('localhost', 1337, 1338);
+try {
+    const Runner = testCafe.createRunner();
+    await Runner
+        .src('./tests/my-fixture.js')
+        .browsers('safari')
+        .run();
+}
+finally {
+    await testCafe.close();
+}
--- pages/loginpage.js
+++ pages/loginpage.js
@@ -1,10 +1,10 @@
 //引入testcafe
-import {
-    Selector
-} from 'testcafe';
+import { Selector} from 'testcafe';
 
+Pagevar = {
 const Logo = Selector('.login_title');
 const username = Selector('#username');
 const password = Selector('#password');
 const vCode = Selector('#vCode');
 const Login = Selector('.loginbtn');
+};
